de6d2dd216818ba1e7c3b49560e75041aca238f3
Ana Mafla <anamafla@hotmail.com>
2019-06-11 14:54:25 -0500

    graphql

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "gatsby-link"
 import Img from "gatsby-image"
-import { graphql } from "gatsby"
+
 import Layout from "../components/layout"
 import { MDBContainer } from "mdbreact"
 
