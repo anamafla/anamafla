c73599073147a06f4473f6f03ac79f76619113a5
Ana Mafla <anamafla@hotmail.com>
2019-06-12 18:12:13 -0500

    conditional window

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,21 +8,24 @@ import { MDBContainer } from "mdbreact"
 export default function Template({ data }) {
   const post = data.markdownRemark
   console.log(post)
-  return (
-    <Layout>
-      <MDBContainer className="mt-5 pt-5">
-        <br />
-        <Link to="/blog">Go Back</Link>
 
-        <h1>{post.frontmatter.title}</h1>
-        <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <p>
-          Posted by {post.frontmatter.author} on {post.frontmatter.date}
-        </p>
-      </MDBContainer>
-    </Layout>
-  )
+  if (typeof window !== `undefined`) {
+    return (
+      <Layout>
+        <MDBContainer className="mt-5 pt-5">
+          <br />
+          <Link to="/blog">Go Back</Link>
+
+          <h1>{post.frontmatter.title}</h1>
+          <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <p>
+            Posted by {post.frontmatter.author} on {post.frontmatter.date}
+          </p>
+        </MDBContainer>
+      </Layout>
+    )
+  }
 }
 
 export const postQuery = graphql`
